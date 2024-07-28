00d06410b732f087ddb518328a2126028260d16f
cuttenEDU <z.zhmakin@gmail.com>
2024-07-28 16:53:13 +0300

    fixed config gen

--- genconfig-vless-xtls-reality.sh
+++ genconfig-vless-xtls-reality.sh
@@ -2,7 +2,9 @@
 echo -n "Enter Reality masking domain: "
 read REALITY_DOMAIN
 
+export REALITY_DOMAIN
 export REALITY_PRIVATE=$(cat private.key)
 export USER_ID=$(cat uuid.txt)
 
-cat templates/xray-config-temp.json | envsubst
+cat templates/xray-config-temp.json | envsubst > /usr/local/etc/xray/config.json
+systemctl restart xray
